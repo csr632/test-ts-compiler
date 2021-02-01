4c206e5962f129afb4d0e99ce8d081b2da159a1a
csr632 <632882184@qq.com>
2021-02-01 19:32:01 +0800

    update

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as dummy from "enhanced-resolve";
-
-// the type is reolved to @types/node,
-// but I expect it to resolve to @types/assert
-import assert from "assert";
-
-// It will resolve correctly if I comment out the first line.
